4e6ab8e231539cd014c585af6be5fe3a26abcf4c
Jacob Young <jacobly0@users.noreply.github.com>
2019-10-27 16:38:38 +0100

    Use correct calling convention from callee.

--- llvm/lib/Target/AMDGPU/AMDGPUCallLowering.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUCallLowering.cpp
@@ -280,7 +280,7 @@ bool AMDGPUCallLowering::lowerReturnVal(MachineIRBuilder &B,
   CCAssignFn *AssignFn = TLI.CCAssignFnForReturn(CC, F.isVarArg());
 
   OutgoingValueHandler RetHandler(B, MF.getRegInfo(), Ret, AssignFn);
-  return handleAssignments(B, SplitRetInfos, RetHandler);
+  return handleAssignments(CC, F.isVarArg(), B, SplitRetInfos, RetHandler);
 }
 
 bool AMDGPUCallLowering::lowerReturn(MachineIRBuilder &B,
--- llvm/lib/Target/ARM/ARMCallLowering.cpp
+++ llvm/lib/Target/ARM/ARMCallLowering.cpp
@@ -256,7 +256,8 @@ bool ARMCallLowering::lowerReturnVal(MachineIRBuilder &MIRBuilder,
       TLI.CCAssignFnForReturn(F.getCallingConv(), F.isVarArg());
 
   OutgoingValueHandler RetHandler(MIRBuilder, MF.getRegInfo(), Ret, AssignFn);
-  return handleAssignments(MIRBuilder, SplitRetInfos, RetHandler);
+  return handleAssignments(F.getCallingConv(), F.isVarArg(), MIRBuilder,
+                           SplitRetInfos, RetHandler);
 }
 
 bool ARMCallLowering::lowerReturn(MachineIRBuilder &MIRBuilder,
@@ -452,7 +453,8 @@ bool ARMCallLowering::lowerFormalArguments(
   if (!MBB.empty())
     MIRBuilder.setInstr(*MBB.begin());
 
-  if (!handleAssignments(MIRBuilder, SplitArgInfos, ArgHandler))
+  if (!handleAssignments(F.getCallingConv(), F.isVarArg(), MIRBuilder,
+                         SplitArgInfos, ArgHandler))
     return false;
 
   // Move back to the end of the basic block.
@@ -548,7 +550,8 @@ bool ARMCallLowering::lowerCall(MachineIRBuilder &MIRBuilder, CallLoweringInfo &
 
   auto ArgAssignFn = TLI.CCAssignFnForCall(Info.CallConv, IsVarArg);
   OutgoingValueHandler ArgHandler(MIRBuilder, MRI, MIB, ArgAssignFn);
-  if (!handleAssignments(MIRBuilder, ArgInfos, ArgHandler))
+  if (!handleAssignments(Info.CallConv, Info.IsVarArg, MIRBuilder, ArgInfos,
+                         ArgHandler))
     return false;
 
   // Now we can add the actual call instruction to the correct basic block.
@@ -562,7 +565,8 @@ bool ARMCallLowering::lowerCall(MachineIRBuilder &MIRBuilder, CallLoweringInfo &
     splitToValueTypes(Info.OrigRet, ArgInfos, MF);
     auto RetAssignFn = TLI.CCAssignFnForReturn(Info.CallConv, IsVarArg);
     CallReturnHandler RetHandler(MIRBuilder, MRI, MIB, RetAssignFn);
-    if (!handleAssignments(MIRBuilder, ArgInfos, RetHandler))
+    if (!handleAssignments(Info.CallConv, Info.IsVarArg, MIRBuilder, ArgInfos,
+                           RetHandler))
       return false;
   }
 
